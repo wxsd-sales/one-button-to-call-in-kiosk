8da13f04a8fd05bc9f2427fcb144122132a3dd1a
Arash Koushkebaghi <arash.koush@gmail.com>
2022-09-23 08:28:18 -0700

    padding issue

---
+++ docs/_app/immutable/chunks/1-9fa44c7a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-6ac2395c.js";export{t as component};
--- docs/_app/immutable/chunks/2-c135f1c4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-c0fc310a.js";export{t as component};
